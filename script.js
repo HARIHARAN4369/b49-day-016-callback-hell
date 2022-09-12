02909b11208870cd1438924d579cd3db3e889b93
HARIHARAN4369 <108862738+HARIHARAN4369@users.noreply.github.com>
2022-09-12 18:33:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+let count=document.querySelector(".count");
+let num=10;
+setTimeout(()=>{
+  count.innerHTML=--num;
+  setTimeout(()=>{
+    count.innerHTML=--num;
+    setTimeout(()=>{
+        count.innerHTML=--num;
+        setTimeout(()=>{
+            count.innerHTML=--num;
+            setTimeout(()=>{
+                count.innerHTML=--num;
+                setTimeout(()=>{
+                    count.innerHTML=--num;
+                    setTimeout(()=>{
+                        count.innerHTML=--num;
+                        setTimeout(()=>{
+                            count.innerHTML=--num;
+                            setTimeout(()=>{
+                                count.innerHTML=--num;
+                                setTimeout(()=>{
+                                    count.innerHTML="L E T S    C E L E B R E A T E";
+                                    setTimeout(()=>{
+                                        count.innerHTML="H A P P Y     I N D E P E N D E N C E     D A Y 💐";
+                                      },1000);
+                                  },1000);
+                              },1000);
+                          },1000);
+                      },1000);
+                  },1000);
+              },1000);
+          },1000);
+      },1000);
+  },1000);
+},1000);
